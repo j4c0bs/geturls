dfa51c796cb415ea5c1b84c95c13899495bab8c5
x <x@classified-records.com>
2017-04-21 23:03:39 -0500

    removed old code used as basis for get_name

--- prep_filename.py
+++ prep_filename.py
@@ -1,32 +1,6 @@
 import os
 import re
 from string import punctuation
-
-# ------------------------------------------------------------------------------
-def num_sfx(xdir, fn, img_type):
-    fn_suffix = 0
-    end_digits = [0]
-    previous_fn = re.compile(r'^%s_\d{2,}\.%s$' % (fn, img_type.strip('.')))
-    for fimg in os.listdir(xdir):
-        if previous_fn.match(fimg):
-            fn_suffix += 1
-            try:
-                znum = int(fimg.rsplit('_',maxsplit=1)[1].split('.')[0])
-                end_digits.append(znum)
-            except ValueError:
-                end_digits.append(0)
-    max_digit = max(end_digits)
-    if max_digit > 0:
-        fn_suffix = max_digit + 1
-    return '_'+str(fn_suffix).rjust(2, '0')
-
-
-def prep_filename(xdir, fn, img_type):
-    img_num = num_sfx(xdir, fn, img_type)
-    if not xdir.endswith(os.sep):
-        xdir += os.sep
-    return ''.join([xdir, fn, img_num, img_type])
-
 # ------------------------------------------------------------------------------
 char_escape = str.maketrans({p: '\{}'.format(p) for p in punctuation})
 
