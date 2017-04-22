358e5861ba7bf0906879f1ba8ff5a86faff4b41c
x <x@classified-records.com>
2017-04-22 02:26:31 -0500

    refactored url to fn,type split

--- prep_filename.py
+++ prep_filename.py
@@ -4,12 +4,17 @@ from string import punctuation
 # ------------------------------------------------------------------------------
 char_escape = str.maketrans({p: '\{}'.format(p) for p in punctuation})
 
-def get_name(filename, root='.'):
+def get_type(filename, subdir=False):
     if '.' not in filename or (filename.startswith('.') and filename.count('.') == 1):
         fn = filename
-        fn_type = ''
+        fn_type = '' if not subdir else 'unknown_filetype'
     else:
         fn, fn_type = filename.rsplit('.', 1)
+    return fn, fn_type
+
+
+def get_name(filename, root=os.curdir):
+    fn, fn_type = get_type(filename)
 
     target_dir = os.listdir(root)
     check_files = [other_fn for other_fn in target_dir if fn in other_fn]
@@ -35,4 +40,19 @@ def get_name(filename, root='.'):
             fn_out = filename
 
     pathname = os.path.join(root, fn_out)
-    return pathname
+    return pathname, fn_out
+
+
+def url_to_path(url, root=os.curdir):
+    filename = url.rsplit('/',1)[1]
+    filepath = os.path.join(root, filename)
+    return filepath, filename
+
+def get_path(url, root=os.curdir, overwrite=False):
+    filename = url.rsplit('/',1)[1]
+    if overwrite:
+        filepath = os.path.join(root, filename)
+    else:
+        filepath, filename = get_name(filename, root)
+
+    return filepath, filename
